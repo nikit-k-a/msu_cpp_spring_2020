5597ead84758a54165117aecfdbf94cfa0d5fe14
nikit-k-a <61841414+nikit-k-a@users.noreply.github.com>
2020-03-11 11:12:42 +0300

    Update Allocator.cpp

--- 01/Allocator.cpp
+++ 01/Allocator.cpp
@@ -7,40 +7,40 @@ static size_t _Bigsize = 0;
 static void* _Start = nullptr;
 
 void makeAllocator (size_t maxSize)
-    {
+{
     _Start = malloc (maxSize);
 
     if(_Start == nullptr)
-        {
+    {
         throw std::exception ();
         return;
-        }
+    }
 
     _Avail   = maxSize;
     _Bigsize = maxSize;
-    }
+}
 
 int* alloc (size_t size)
-    {
+{
     if (size > _Avail)
-        {
+    {
         return nullptr;
-        }
+    }
 
     else
-        {
+    {
         _Start = (int*)(_Start) + size;
         _Avail -= size;
 
         return (int*)_Start;
-        }
-
-    return nullptr;
     }
 
+    return nullptr;
+}
+
 void reset ()
-    {
+{
     _Avail = _Bigsize;
 
     _Start = (int*)(_Start) - (_Bigsize - _Avail);
-    }
+}
